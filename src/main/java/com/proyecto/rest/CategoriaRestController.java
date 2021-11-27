fcdd0136256f1ea9a84f5a8848501549be32fea6
bruno <bruno@DESKTOP-IPLR7MD>
2021-11-26 22:51:38 -0500

    categoria

--- src/main/java/com/proyecto/rest/CategoriaRestController.java
+++ src/main/java/com/proyecto/rest/CategoriaRestController.java
@@ -1,16 +1,20 @@
 package com.proyecto.rest;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,12 +25,14 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/categorias")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CategoriaRestController {
 
 	@Autowired
 	private CategoriasServices service;
 
 	@GetMapping
+	@ResponseBody
 	public List<Categorias> listar() {
 		return service.listar();
 	}
@@ -44,6 +50,7 @@ public class CategoriaRestController {
 				.buildAndExpand(categoriaGuardado.getIdcat()).toUri();
 		return ResponseEntity.created(location).build();
 	}
+
 
 	@PutMapping("{idcli}")
 	public ResponseEntity<Object> actualizar(@RequestBody Categorias categorias, @PathVariable Long idcat) {
